d6e0f1a018f37a4988bf83fa0d755355af714f6e
Zero-atom <atom.vip.gold@gmail.com>
2024-04-14 23:13:49 +0500

    Spring Boot Actuator

--- src/main/java/tacos/controller/TacoController.java
+++ src/main/java/tacos/controller/TacoController.java
@@ -1,4 +1,22 @@
 package tacos.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tacos.Taco;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequiredArgsConstructor
 public class TacoController {
+
+//    private final TacoService tacoService;
+//
+//    @GetMapping
+//    public List<Taco> getTacos() {
+//        return tacoService.getAll();
+//    }
 }
